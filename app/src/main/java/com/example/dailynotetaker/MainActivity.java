211e39ac5c7547bae1e29daa91182629a2b7a3df
Saptarshi Pal <palsreturn2@gmail.com>
2020-04-10 10:33:09 +0530

    Recycler view working intitial stage

--- app/src/main/java/com/example/dailynotetaker/MainActivity.java
+++ app/src/main/java/com/example/dailynotetaker/MainActivity.java
@@ -9,6 +9,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
@@ -16,14 +18,35 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView displayBox;
+    private RecyclerView.Adapter madapter;
+    private RecyclerView.LayoutManager layoutM;
+    ArrayList<String> myDataset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        try
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            displayBox = (RecyclerView)findViewById(R.id.displayBox);
+            displayBox.setHasFixedSize(true);
+            layoutM = new LinearLayoutManager(this);
+            displayBox.setLayoutManager(layoutM);
+            myDataset = new ArrayList<String>();
+            madapter = new MyAdapter(myDataset);
+            displayBox.setAdapter(madapter);
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG);
+        }
+
     }
 
     @Override
@@ -51,6 +74,9 @@ public class MainActivity extends AppCompatActivity {
     {
         Context context = getApplicationContext();
         EditText textinput = (EditText)findViewById(R.id.textInput);
-        Toast.makeText(context,textinput.getText().toString(), Toast.LENGTH_LONG).show();
+        String txtin = textinput.getText().toString();
+        myDataset.add(txtin);
+        Toast.makeText(context, "Saved", Toast.LENGTH_LONG).show();
+
     }
 }
---
+++ app/src/main/java/com/example/dailynotetaker/MyAdapter.java
@@ -0,0 +1,48 @@
+package com.example.dailynotetaker;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    public ArrayList<String> mDataset;
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView textView;
+        public MyViewHolder(TextView v)
+        {
+            super(v);
+            textView = v;
+        }
+    }
+    public MyAdapter(ArrayList<String> myDataset)
+    {
+        mDataset = myDataset;
+    }
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        TextView v = (TextView)LayoutInflater.from(parent.getContext()).inflate(R.layout.support_simple_spinner_dropdown_item, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position)
+    {
+        holder.textView.setText(mDataset.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
